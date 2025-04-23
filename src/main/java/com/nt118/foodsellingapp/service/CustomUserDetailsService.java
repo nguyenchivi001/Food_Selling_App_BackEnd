a5ae6ecde31836018190ab5435d159710b928cd1
nguyenchivi001 <nguyenchivi292004@gmail.com>
2025-04-23 22:41:29 +0700

    config jwt for authentication and authorization

---
+++ src/main/java/com/nt118/foodsellingapp/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.nt118.foodsellingapp.service;
+
+import com.nt118.foodsellingapp.dao.UserRepository;
+import com.nt118.foodsellingapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
