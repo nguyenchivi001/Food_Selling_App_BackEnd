5792e628328ce2fafac0c0d0738fd614ff049dc3
nguyenchivi001 <nguyenchivi292004@gmail.com>
2025-04-23 15:23:21 +0700

    init commit

---
+++ src/main/java/com/nt118/foodsellingapp/FoodsellingappApplication.java
@@ -0,0 +1,13 @@
+package com.nt118.foodsellingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsellingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsellingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nt118/foodsellingapp/FoodsellingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt118.foodsellingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsellingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
